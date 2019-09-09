d53e69ad973a94c7bf3520d5f32108dcd2446c69
wenfujing <3131713955@qq.com>
2019-09-09 14:54:52 +0800

    first commit

--- app/src/main/java/com/mumu/jsguidepage/GuidePageActivity.java
+++ app/src/main/java/com/mumu/jsguidepage/GuidePageActivity.java
@@ -1,21 +1,29 @@
 package com.mumu.jsguidepage;
 
-import android.content.Context;
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.bigkoo.convenientbanner.ConvenientBanner;
 import com.bigkoo.convenientbanner.holder.CBViewHolderCreator;
 import com.bigkoo.convenientbanner.holder.Holder;
 import com.bigkoo.convenientbanner.listener.OnPageChangeListener;
 
+import java.lang.ref.WeakReference;
 import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -23,9 +31,8 @@ import butterknife.OnClick;
 import butterknife.Unbinder;
 
 /**
- * author : zlf
- * date   : 2018/11/26
- * blog   :https://www.jianshu.com/u/281e9668a5a6
+ * author : wfj
+ * date   : 2019/9.9
  */
 public class GuidePageActivity extends AppCompatActivity {
 
@@ -36,29 +43,42 @@ public class GuidePageActivity extends AppCompatActivity {
     Button btnTest;
 
     private ArrayList<Integer> arrayList;
-
+    /**倒计时文本*/
+    private TextView mCountdownTextView;
+    private static final int NUM = 9;
+    private int countdownNum;//倒计时的秒数
+    private MyHandler countdownHandle;//用于控制倒计时子线程
+    private Runnable runnable;//倒计时子线程
+    private static Timer timer;//计时器
+    private static final int MSG_COUNT_WHAT = 99;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        /*==========设置全屏======必须在setContentView前面=======*/
+        /*set it to be no title*/
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        /*set it to be full screen*/
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_guide_page);
-        unbinder = ButterKnife.bind(this);
-        initView();
-        initGuidePage();
-    }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        unbinder.unbind();
+        unbinder = ButterKnife.bind(this);
+        //初始化控件
+        initView();
+        //初始化底部圆点
+        initGuidePage();
+        //初始化Handler和Runnable
+        initThread();
     }
 
     private void initView() {
+        mCountdownTextView = (TextView) findViewById(R.id.id_countdownTextView);
+        //引导欢迎界面
         arrayList = new ArrayList<>();
         arrayList.add(R.mipmap.b1);
         arrayList.add(R.mipmap.b2);
         arrayList.add(R.mipmap.b3);
     }
-
+    //底部圆点
     private void initGuidePage() {
         cbTest.setPages(new CBViewHolderCreator() {
             @Override
@@ -71,7 +91,6 @@ public class GuidePageActivity extends AppCompatActivity {
                 //设置加载哪个布局
                 return R.layout.item_guide_page;
             }
-
         }, arrayList)
                 .setPageIndicator(new int[]{R.mipmap.ic_page_indicator, R.mipmap.ic_page_indicator_focused})
                 .setPageIndicatorAlign(ConvenientBanner.PageIndicatorAlign.CENTER_HORIZONTAL)
@@ -102,12 +121,89 @@ public class GuidePageActivity extends AppCompatActivity {
                     }
                 });
     }
+    /**
+     * 初始化Handler和Runnable
+     * */
+    private void initThread(){
+        //倒计时变量
+        initCountdownNum();
+        //handler对象
+        countdownHandle = new MyHandler(this);
+        //runnable
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                //执行倒计时代码
+                timer = new Timer();
+                TimerTask task = new TimerTask() {
+                    public void run() {
+                        countdownNum --;
 
-    @OnClick(R.id.btn_test)
-    public void onViewClicked() {
-        //跳转到主activity
-        Intent intent = new Intent(GuidePageActivity.this, MainActivity.class);
-        startActivity(intent);
+                        Message msg = countdownHandle.obtainMessage();
+                        msg.what = MSG_COUNT_WHAT;//message的what值
+                        msg.arg1 = countdownNum;//倒计时的秒数
+
+                        countdownHandle.sendMessage(msg);
+                    }
+                };
+                timer.schedule(task,0,1000);
+            }
+        };
+    }
+
+    /**必须使用静态类：
+     * 解决问题：This Handler class should be static or leaks might occur Android
+     * http://www.cnblogs.com/jevan/p/3168828.html*/
+    private class MyHandler extends Handler {
+        // WeakReference to the outer class's instance.
+        private WeakReference<GuidePageActivity> mOuter;
+
+        public MyHandler(GuidePageActivity activity) {
+            mOuter = new WeakReference<GuidePageActivity>(activity);
+        }
+        @Override
+        public void handleMessage(Message msg) {
+            GuidePageActivity theActivity = mOuter.get();
+            if (theActivity != null) {
+
+                switch (msg.what) {
+                    case MSG_COUNT_WHAT:
+                        if(msg.arg1 == 0){//表示倒计时完成
+
+                            //在这里执行的话，不会出现-1S的情况
+                            if(timer != null){
+                                timer.cancel();//销毁计时器
+                            }
+                            openNextActivity(GuidePageActivity.this);//打开下一个界面
+//                            openNextActivity(theActivity);
+
+
+                        }else{
+
+                            theActivity.mCountdownTextView.setText("跳过" + msg.arg1 + "s");
+                        }
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+
+    @OnClick({R.id.btn_test,R.id.id_countdownTextView})
+    public void onViewClicked(View view) {
+        stopThread();
+        openNextActivity(GuidePageActivity.this);//打开下一个界面
+    }
+
+    //打开下一个界面
+    private static void openNextActivity(Activity mActivity) {
+        //跳转到登录界面并销毁当前界面
+        Intent intent = new Intent(mActivity, MainActivity.class);
+        mActivity.startActivity(intent);
+
+        mActivity.finish();
     }
 
     /**
@@ -131,5 +227,43 @@ public class GuidePageActivity extends AppCompatActivity {
         public void updateUI(Integer data) {
             mImageView.setImageResource(data);
         }
+    }
+    @Override
+    protected void onResume() {
+        //开启线程
+        countdownHandle.post(runnable);
+        super.onResume();
+    }
+
+    @Override
+    protected void onStop() {
+
+        initCountdownNum();//初始化倒计时的秒数，这样按home键后再次进去欢迎界面，则会重新倒计时
+
+        stopThread();
+
+        super.onStop();
+    }
+
+    //停止倒计时
+    private void stopThread(){
+        //在这里执行的话，用户点击home键后，不会继续倒计时进入登录界面
+        if(timer != null){
+            timer.cancel();//销毁计时器
+        }
+
+        //将线程销毁掉
+        countdownHandle.removeCallbacks(runnable);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unbinder.unbind();
+    }
+
+    /*初始化倒计时的秒数*/
+    private void initCountdownNum(){
+        countdownNum = NUM;
     }
 }
